9ceebe27755038f90c88bb9b10a6cd2da312e6b5
mrahman13 <116874917+mrahman13@users.noreply.github.com>
2023-03-10 21:27:45 +0000

    test

---
+++ pages/navbar.php
@@ -0,0 +1,8 @@
+<?php
+  echo '<nav class="nav">';
+  echo '<ul>';
+  echo '<li><a href="index.php">Performance dashboard</a></li>';
+  echo '<li><a href="dashboard.php">Dashboard</a></li>';
+  echo '</ul>';
+  echo '</nav>';
+?>
