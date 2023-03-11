fbc0ba981f3b6424d3c0c31a089138722883375f
tomljo2000 <66169128+tomljo2000@users.noreply.github.com>
2023-03-11 00:23:24 +0000

    fixed navbar  too many echo

--- pages/navbar.php
+++ pages/navbar.php
@@ -1,8 +1,6 @@
-<?php
-  echo '<nav class="nav">';
-  echo '<ul>';
-  echo '<li><a href="index.php">Performance dashboard</a></li>';
-  echo '<li><a href="dashboard.php">Dashboard</a></li>';
-  echo '</ul>';
-  echo '</nav>';
-?>
+<nav class="nav">
+  <ul>
+    <li><a href="index.php">Performance dashboard</a></li>
+    <li><a href="dashboard.php">Dashboard</a></li>
+  </ul>
+</nav>
