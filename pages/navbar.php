cff67e07a58235c05e7aacd2b8cbbfb71b5472d9
tomljo2000 <66169128+tomljo2000@users.noreply.github.com>
2023-03-11 17:47:01 +0000

    nav update

--- pages/navbar.php
+++ pages/navbar.php
@@ -1,18 +1,25 @@
-<?php require 'index.php';?>
+<?php require 'index.php'; ?>
 
 <nav class="nav">
-<!-- <form action="overview.php" method="POST">
-  <input type="submit"/>
-</form>
-<form action="dashboard.php" method="POST">
-  <input type="submit"/>
-</form> -->
+  <div class="nav-content">
 
-<a href="overview.php">
-  <button>Overview</button>
-</a>
-<a href="dashboard.php">
-  <button>Dashboard</button>
-</a>
+    <h1>Waterman Grp</h1>
 
+  </div>
+  <div class="nav-content">
+
+    <h4>Enviromental</h4>
+
+  </div>
+  <div class="nav-content">
+
+    <a href="overview.php">
+      <button class="nav-item">Overview</button>
+    </a>
+    <a  href="dashboard.php">
+      <button class="nav-item" >Dashboard</button>
+    </a>
+
+  </div>
+  </div>
 </nav>
