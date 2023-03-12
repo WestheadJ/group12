28ea77d08d6e360537b539709c6b3ca15af6152f
tomljo2000 <66169128+tomljo2000@users.noreply.github.com>
2023-03-12 20:53:10 +0000

    Mongoloid merge

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -5,20 +5,14 @@
 -->
 
 <?php require 'navbar.php' ?>
-<script src="../scripts/addGraphBtn.js"></script>
-<div id="mainpage">
-  
-  <div class="mainpage-row" id="row-1">
-    <div class="mainpage-row-item" id="row-1-item-1">
-      <canvas id="chart"></canvas>
-    </div>
-
-    <div class="mainpage-row-item">
-      <div class="add-graph-button">
-        <!-- <a href="chartSelect.php"> -->
-          <button onclick="buttonClickityClick()">Add</button>
-        <!-- </a> -->
-      </div>
-    </div>
-  </div>
-</div>
+ <script src="../scripts/addGraphBtn.js"></script>
+ <div id="mainpage">
+   <div class="mainpage-row" id="row-1">
+     <div class="mainpage-row-item" id="row-1-item-1">
+       <canvas id="guageChart"></canvas>
+     </div>
+     <div class="mainpage-row-item" id="row-1-item-1">
+       <canvas id="lineChart"></canvas>
+     </div>
+   </div>
+ </div>
