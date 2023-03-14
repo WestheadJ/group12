163dfd3cfe3906e72e5939d9ae5485377cc736d5
mRichardio <c2019629@hallam.shu.ac.uk>
2023-03-14 00:12:36 +0000

    Added 2 more options for Multilevel Pie Charts

--- pages/fancyPie.php
+++ pages/fancyPie.php
@@ -13,14 +13,37 @@
 <script src="../scripts/addGraphBtn.js"></script>
 <div id="mainpage">
 
-  <div class="mainpage-row">
-    <div class="mainpage-row-item">
-        <script src="https://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.js"></script>
-        <script src="https://cdn.fusioncharts.com/fusioncharts/latest/themes/fusioncharts.theme.fusion.js"></script>
-        <div id="chart-container">FusionCharts will render here</div>
-        <script src="../scripts/multiPieChart.js"></script>
+    <div class="mainpage-row">
+
+      <!-- Chart Option #1  -->
+      <div class="mainpage-row-item">
+          <div id="chart-container">FusionCharts will render here</div>
+          <script src="../scripts/multiPieChart.js"></script>
+      </div>
+
+      <!-- Chart Option #2  -->
+      <div class="mainpage-row-item">
+        <label class="sun"><input onclick="interactivitySelectionMode('drill-down')" type="radio" name="mode">drill-down</label>
+        <label class="sun"><input onclick="interactivitySelectionMode('multi-select')" type="radio" name="mode">multi-select</label>
+        <label class="sun"><input onclick="interactivitySelectionMode('single-select')" type="radio" name="mode">single-select</label>
+        <label class="sun"><input onclick="interactivitySelectionMode('none')" type="radio" name="mode" checked>none</label>
+        <div id="container">
+          <script src="https://cdn.anychart.com/releases/8.11.0/js/anychart-base.min.js?hcode=a0c21fc77e1449cc86299c5faa067dc4"></script>
+          <script src="https://cdn.anychart.com/releases/8.11.0/js/anychart-sunburst.min.js?hcode=a0c21fc77e1449cc86299c5faa067dc4"></script>
+          <script src="https://cdn.anychart.com/releases/8.11.0/js/anychart-exports.min.js?hcode=a0c21fc77e1449cc86299c5faa067dc4"></script>
+          <script src="https://cdn.anychart.com/releases/8.11.0/js/anychart-ui.min.js?hcode=a0c21fc77e1449cc86299c5faa067dc4"></script>
+          <script src="../scripts/sunBurstChart.js"></script>
+        </div>
+      </div>
+
+      <!-- Chart Option #3  -->
+      <div class="mainpage-row-item">
+        <div id='myDiv'>
+        <script src='https://cdn.plot.ly/plotly-2.18.2.min.js'></script>
+          <script src='../scripts/testSunBurstChart.js'></script>
+        </div>
+      </div>
     </div>
-  </div>
 
   <div class = "abutton">
     <button onclick="buttonClickityClick()">+</button>
@@ -34,3 +57,7 @@
     </div>
   </div>
 </div>
+
+
+
+
--- pages/index.php
+++ pages/index.php
@@ -7,6 +7,10 @@
   <title>Gauge Chart</title>
   <script src="https://unpkg.com/chart.js@2.8.0/dist/Chart.bundle.js"></script>
   <script src="https://unpkg.com/chartjs-gauge@0.3.0/dist/chartjs-gauge.js"></script>
+  <script src="https://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.js"></script>
+  <script src="https://cdn.fusioncharts.com/fusioncharts/latest/themes/fusioncharts.theme.fusion.js"></script>
+  <link href="https://cdn.anychart.com/releases/8.11.0/css/anychart-ui.min.css?hcode=a0c21fc77e1449cc86299c5faa067dc4" rel="stylesheet" type="text/css">
+  <link href="https://playground.anychart.com/docs/samples/BCT_Sunburst_Chart_19/iframe" rel="canonical">
   <link rel="stylesheet" href="../styles/style.css">
   <link rel="stylesheet" href="../styles/navbar.css">
   <script src="../scripts/guageChart.js"></script>
