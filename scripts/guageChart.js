ced4d7635642a23b85ef6748330222f9f4688b99
James Westhead <james_westhead@outlook.com>
2023-03-15 01:26:12 +0000

    Read chart JSON configs, allows to insert the graphs

--- scripts/guageChart.js
+++ scripts/guageChart.js
@@ -53,43 +53,14 @@ window.onload = function () {
   
 };
 
-var config = {
-  type: 'gauge',
-  data: {
-    //labels: ['Success', 'Warning', 'Warning', 'Error'],
-    datasets: [{
-      data: data,
-      value: value,
-      backgroundColor: ['green', 'orange', 'red'],
-      borderWidth: 2
-    }]
-  },
-  options: {
-    responsive: true,
-    title: {
-      display: true,
-      text: 'Gauge chart'
-    },
-    layout: {
-      padding: {
-        bottom: 30
-      }
-    },
-    needle: {
-      // Needle circle radius as the percentage of the chart area width
-      radiusPercentage: 2,
-      // Needle width as the percentage of the chart area width
-      widthPercentage: 3.2,
-      // Needle length as the percentage of the interval between inner radius (0%) and outer radius (100%) of the arc
-      lengthPercentage: 80,
-      // The color of the needle
-      color: 'rgba(0, 0, 0, 1)'
-    },
-    valueLabel: {
-      formatter: Math.round
-    }
-  }
-};
+var config;
+
+fetch('../graphs/guage.json')
+    .then((response) => response.json())
+    .then((json) =>{ 
+    console.log(json)
+    config = json
+    config.value = value});
 
 var config1 = {
   type: 'bar',
