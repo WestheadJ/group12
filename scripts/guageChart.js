d060839fba48c3b2ddc27284a97afcc65490bff3
James Westhead <james_westhead@outlook.com>
2023-03-15 02:29:41 +0000

    Adds label

--- scripts/guageChart.js
+++ scripts/guageChart.js
@@ -33,10 +33,15 @@ window.onload = async function () {
     gaugeConfigs = await fetch('../configs/graphs/guage.json')
     .then((response) => response.json())
     .then((json) =>{ 
-    json.data.datasets[0].data=data
+  json.data.datasets[0].data=data
     json.data.datasets[0].value = value
-    json.options.valueLabel = Math.round
+    json.options.valueLabel.formatter = Math.round
+      
 
+    // DEBUG 
+    console.log("data:",json.data.datasets[0].data=data)
+    console.log("value",json.data.datasets[0].value = value)
+    
     var ctx = document.getElementById('guageChart').getContext('2d');
     window.guageChart = new Chart(ctx, json);
     return json
@@ -74,43 +79,43 @@ window.onload = async function () {
   
 };
 
-// var json = {
-//   type: "gauge",
-//   data: {
-
-//     datasets: [{
-//       data: data,
-//       value: value,
-//       backgroundColor: ["green", "orange", "red"],
-//       borderWidth: 2
-//     }]
-//   },
-//   options: {
-//     responsive: true,
-//     title: {
-//       display: true,
-//       text: "Gauge chart"
-//     },
-//     layout: {
-//       padding: {
-//         bottom: 30
-//       }
-//     },
-//     needle: {
-
-//       radiusPercentage: 2,
-
-//       widthPercentage: 3.2,
-
-//       lengthPercentage: 80,
-
-//       color: "rgba(0, 0, 0, 1)"
-//     },
-//     valueLabel: {
-//       formatter: Math.round
-//     }
-//   }
-// }
+var config = {
+  type: 'gauge',
+  data: {
+    //labels: ['Success', 'Warning', 'Warning', 'Error'],
+    datasets: [{
+      data: data,
+      value: value,
+      backgroundColor: ['green', 'orange', 'red'],
+      borderWidth: 2
+    }]
+  },
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: 'Gauge chart'
+    },
+    layout: {
+      padding: {
+        bottom: 30
+      }
+    },
+    needle: {
+      // Needle circle radius as the percentage of the chart area width
+      radiusPercentage: 2,
+      // Needle width as the percentage of the chart area width
+      widthPercentage: 3.2,
+      // Needle length as the percentage of the interval between inner radius (0%) and outer radius (100%) of the arc
+      lengthPercentage: 80,
+      // The color of the needle
+      color: 'rgba(0, 0, 0, 1)'
+    },
+    valueLabel: {
+      formatter: Math.round
+    }
+  }
+};
 
 var config2 = {
   type: 'pie',
