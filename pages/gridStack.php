e1ce5e1123dcb6720f4b1bb5b19c034181b7856c
tomljo2000 <66169128+tomljo2000@users.noreply.github.com>
2023-03-19 18:47:19 +0000

    Stack working, looks bad needs work

--- pages/gridStack.php
+++ pages/gridStack.php
@@ -1,6 +1,8 @@
 <?php require 'index.php'; ?>
-<script src=".../node_modules/gridstack/dist/gridstack-h5.js"></script>
-<link href=".../node_modules/gridstack/dist/gridstack.min.css" rel="stylesheet" />
+<!-- <script src="http://localhost/greenSpace/group12/node_modules/gridstack/dist/gridstack-h5.js"></script>
+<link href=" http://localhost/greenSpace/group12/node_modules/gridstack/dist/gridstack.min.css" rel="stylesheet" /> -->
+<link href="http://localhost/greenSpace/group12/node_modules/gridstack/dist/gridstack.min.css" rel="stylesheet"/>
+<script src="http://localhost/greenSpace/group12/node_modules/gridstack/dist/gridstack-all.js"></script>
 <style type="text/css">
     .grid-stack {
         background: #FAFAD2;
@@ -11,18 +13,15 @@
     }
 </style>
 
-<div class="grid-stack"></div>
+<div class="grid-stack">
+  <div class="grid-stack-item">
+    <div class="grid-stack-item-content">Item 1</div>
+  </div>
+  <div class="grid-stack-item" gs-w="2">
+    <div class="grid-stack-item-content">Item 2 wider</div>
+  </div>
+</div>
 
 <script type="text/javascript">
-    require('gridstack');
-    var items = [{
-            content: 'my first widget'
-        }, // will default to location (0,0) and 1x1
-        {
-            w: 2,
-            content: 'another longer widget!'
-        } // will be placed next at (1,0) and 2x1
-    ];
-    var grid = GridStack.init();
-    grid.load(items);
+    GridStack.init();
 </script>
