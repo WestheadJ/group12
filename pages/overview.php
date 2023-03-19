0fc69763a8d9ec6fbf78629deff203ff54de2316
mRichardio <c2019629@hallam.shu.ac.uk>
2023-03-19 14:54:03 +0000

    Added HTML to overview page for multi graph

--- pages/overview.php
+++ pages/overview.php
@@ -1,3 +1,13 @@
 <?php 
-    require 'navbar.php'; 
+require 'navbar.php'; 
 ?>
+
+<div id="mainpage">
+    <div class="mainpage-row">
+        <div class="mainpage-row-item">
+            <div id="chart-container">Overview chart will render here</div>
+                <script src="../scripts/overviewPieChart.js"></script>
+            </div>
+        </div>
+    </div>
+</div>
