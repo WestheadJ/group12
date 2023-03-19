52db42e3ebf1d30ce0e7404b8d2103fc4a770254
James Westhead <james_westhead@outlook.com>
2023-03-19 00:40:31 +0000

    Css is not working rn

--- scripts/charts.js
+++ scripts/charts.js
@@ -19,28 +19,33 @@ var randomValue = function (data) {
 var data = randomData();
 var value = randomValue(data);
 
+
 window.onload = () => {
+
+  // --- Create the layout of the dashboard ---
+
+  // Gets the config file using the JS Fetch API (It's a promise function)
+  // Promises: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
   fetch("../configs/dashboards/dashboard.json")
+  // Gets the data and converts it to a JSON object to work with
   .then(response=>response.json())
+  // Render function
   .then((json)=>{
 
     // Get the render container
     var renderContainer = document.getElementById('render')
     
-    var rowID
-    var rowEl
-    
-    // For each row in the dashboard
+    // *For each row* in the dashboard ->  
     json.configs.forEach(row=>{
       
       // DEBUG
       // console.log("This is row:",row.rowID.toString())
 
-      // Get the rowID
-      rowID = row.rowID.toString()
+      // Gets the rowID (Converts to string as in the JSON it is an int)
+      var rowID = row.rowID.toString()
       
       // Ready a div to become a row
-      rowEl = document.createElement("div")
+      var rowEl = document.createElement("div")
       
       // Give it a class and an id
       rowEl.setAttribute("class","mainpage-row")
@@ -49,7 +54,7 @@ window.onload = () => {
       // Add div to the container
       renderContainer.appendChild(rowEl)
       
-      // Loop through the rows
+      // *For each column* in the row ->
       row.columns.forEach(column=>{
         
         // DEBUGS
@@ -57,40 +62,45 @@ window.onload = () => {
         // console.log("This is column",column.colID.toString(),"of row",rowID )
         // console.log("GraphID =",column.graphID)
         
-        // Get the column ID to a string
+        // Get the column ID (Needs converting to a string because it's an int)
         var colID = column.colID.toString()
-        // Get the graphs ID
 
-        // Get the row div that you are working in
+        // Get the current rows div
         var insertRowEl = document.getElementById("row-id-"+rowID)
         
         // Prepare a canvas element
         var columnEl = document.createElement("canvas")
         
-        // Give canvas a class and ID
+        // Give prepares canvas a class and ID
         columnEl.setAttribute("class","mainpage-row-item")
-        columnEl.setAttribute("id","row-id-"+rowID+"col-id-"+colID)
+        columnEl.setAttribute("id","row-id-"+rowID+"-col-id-"+colID)
         
+        // Add to the row
         insertRowEl.appendChild(columnEl)
       })
     })
   })
 
+
+  // --- Add the graphs to the dashboard --- 
+
+  // Gets the config file using the JS Fetch API
   fetch("../configs/dashboards/dashboard.json")
+  // Gets the data and converts it to a JSON object to work with
   .then(response=>response.json())
+  // Render function
   .then((json)=>{
-    var rowID
-    
-    // For each row in the dashboard
+
+    // *For each row* in the dashboard ->  
     json.configs.forEach(row=>{
       
-    // DEBUG
+    // DEBUG:
     // console.log("This is row:",row.rowID.toString())
 
     // Get the rowID
-    rowID = row.rowID.toString()
+    var rowID = row.rowID.toString()
       
-    // Loop through the rows
+    // *For each column* in the row ->
     row.columns.forEach(column=>{
         
       // DEBUGS
@@ -98,17 +108,22 @@ window.onload = () => {
       // console.log("This is column",column.colID.toString(),"of row",rowID )
       // console.log("GraphID =",column.graphID)
         
-      // Get the column ID to a string
+      // Get the column ID
       var colID = column.colID.toString()
 
-      // console.log("row-id-"+rowID+"col-id-"+colID)
-      // Get the graphs ID
-      var canvas  = document.getElementById("row-id-"+rowID+"col-id-"+colID)
+      // Get the canvas element using it's rowID and colID
+      var canvas  = document.getElementById("row-id-"+rowID+"-col-id-"+colID)
 
+      // If the graphs ID is gauge get the gauge data (This is because the gauge data has the value section)
       if(column.graphID==="gauge"){
+
+        // Get the graphs config using JS Fetch API
         fetch('../configs/graphs/gauge.json')
+        // Gets the data and converts it to a JSON object to work with
         .then((response) => response.json())
+        // Render function
         .then((gaugeData) =>{ 
+          // Prepare the configs for the graphs
           gaugeData.data.datasets[0].data=data
           gaugeData.data.datasets[0].value = value
           gaugeData.options.valueLabel.formatter = Math.round 
@@ -117,90 +132,30 @@ window.onload = () => {
           // console.log()
           // console.log("data:",gaugeData.data.datasets[0].data=data)
           // console.log("value",gaugeData.data.datasets[0].value = value)
-          console.log(canvas)
+
+          // Render the graph
           var ctx = canvas.getContext('2d');
           return window.guageChart = new Chart(ctx, gaugeData);
         })
       }
-      
+      else{
+        // Get the graphs config using JS Fetch API
+        fetch(`../configs/graphs/${column.graphID}.json`)
+        // Gets the data and converts it to a JSON object to work with
+        .then(response=>response.json())
+        // Render function
+        .then(graphData=>{
+          // Prepare the configs for the graphs
+          graphData.data.datasets[0].data = data
+          
+          // Render the graph
+          var ctx = canvas.getContext('2d');
+          return window.graph = new Chart(ctx, graphData)
+        })
+      }
       
       })
     })
   })
   
-
-
-
-  // // https://www.freecodecamp.org/news/how-to-read-json-file-in-javascript/w
-
-  // // Getting the gauge
-  // fetch('../configs/graphs/gauge.json')
-  //   .then((response) => response.json())
-  //   .then((json) =>{ 
-  //   json.data.datasets[0].data=data
-  //   json.data.datasets[0].value = value
-  //   json.options.valueLabel.formatter = Math.round 
-
-  //   // DEBUG 
-    // console.log(json)
-  //   // console.log("data:",json.data.datasets[0].data=data)
-  //   // console.log("value",json.data.datasets[0].value = value)
-    
-  //   var ctx = document.getElementById('guageChart').getContext('2d');
-  //   window.guageChart = new Chart(ctx, json);
-
-
-  //   ctx = document.getElementById('guageChartR2').getContext('2d');
-  //   window.guageChart = new Chart(ctx, json);
-    
-
-  //   ctx = document.getElementById('guageChartR4').getContext('2d');
-  //   return window.guageChart = new Chart(ctx, json);
-  // });
-
-
-  // fetch('../configs/graphs/bar.json')
-  //     .then((response) => response.json())
-  //     .then((json) =>{ 
-      
-  //       // DEBUG
-  //     // console.log(json)
-      
-  //     json.data.datasets[0].data = data
-
-  //     var ctx = document.getElementById('barChartR2').getContext('2d');
-  //     window.lineChart = new Chart(ctx, json);
-
-  //     ctx = document.getElementById('barChartR3').getContext('2d');
-  //     window.lineChart = new Chart(ctx, json);
-
-  //     ctx = document.getElementById('barChart').getContext('2d');
-  //     return window.lineChart = new Chart(ctx, json);
-  //   });
-
-  //   fetch('../configs/graphs/pie.json')
-  //   .then((response)=>response.json())
-  //   .then((json)=>{
-  //     json.data.datasets[0].data=data
-
-  //     var ctx = document.getElementById('pieChart').getContext('2d');
-  //     window.lineChart = new Chart(ctx, json);var 
-      
-  //     ctx = document.getElementById('pieChartR2').getContext('2d');
-  //     window.lineChart = new Chart(ctx, json);
-  
-  //     ctx = document.getElementById('pieChartR3').getContext('2d');
-  //     return window.lineChart = new Chart(ctx, json);
-
-  //   })
-
-  //   fetch("../configs/graphs/line.json")
-  //   .then(response=>response.json())
-  //   .then((json)=>{
-  //     json.data.datasets[0].data = data  
-      
-  //     var ctx = document.getElementById('lineChart').getContext('2d');
-  //     return window.lineChart = new Chart(ctx, json);
-  //   })
-
   };
