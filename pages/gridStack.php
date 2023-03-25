16ee8cff40ec06f9678865c6358129f41b58704c
tomljo2000 <66169128+tomljo2000@users.noreply.github.com>
2023-03-25 15:57:27 +0000

    Debugging attempts
    Read comments in gridStack.php.

--- pages/gridStack.php
+++ pages/gridStack.php
@@ -1,16 +1,30 @@
 <?php require 'navbar.php'; ?>
-<!-- <script src="http://localhost/greenSpace/group12/node_modules/gridstack/dist/gridstack-h5.js"></script>
-<link href=" http://localhost/greenSpace/group12/node_modules/gridstack/dist/gridstack.min.css" rel="stylesheet" /> -->
-<link href="../node_modules/gridstack/dist/gridstack.min.css" rel="stylesheet"/>
+<link href="../node_modules/gridstack/dist/gridstack.min.css" rel="stylesheet" />
 <script src="../node_modules/gridstack/dist/gridstack-all.js"></script>
 <style type="text/css">
     .grid-stack {
         background: #FAFAD2;
     }
 </style>
-
+<!-- Why does this line code not work, It prints to the html in the form below!!!! Just doesn't render-->
 <div id="render"></div>
 
+<!-- THIS WORKS -->
+<!-- <div class="grid-stack">
+    <div class="grid-stack-item" gs-w="3">
+        <div class="grid-stack-item-content">{"type":"bar","data":{"labels":["Green","Amber","Red"],"datasets":[{"label":"Chart","data":"data","backgroundColor":["green","orange","red"],"borderWidth":1}]},"options":{"scales":{"yAxes":[{"ticks":{"beginAtZero":true}}]}}}</div>
+    </div>
+    <div class="grid-stack-item" gs-w="3">
+        <div class="grid-stack-item-content">{"type":"gauge","data":{"datasets":[{"data":"data","value":"value","backgroundColor":["green","orange","red"],"borderWidth":2}]},"options":{"responsive":true,"title":{"display":true,"text":"Gauge chart"},"layout":{"padding":{"bottom":30}},"needle":{"radiusPercentage":2,"widthPercentage":3.2,"lengthPercentage":80,"color":"rgba(0, 0, 0, 1)"},"valueLabel":{"formatter":"Math.round"}}}</div>
+    </div>
+    <div class="grid-stack-item" gs-w="3">
+        <div class="grid-stack-item-content">{"type":"line","data":{"labels":["Green","Amber","Red"],"datasets":[{"label":"Chart","data":"data","fill":false,"backgroundColor":["green","orange","red"],"borderWidth":1}]},"options":{"scales":{"yAxes":[{"ticks":{"beginAtZero":true}}]}}}</div>
+    </div>
+    <div class="grid-stack-item" gs-w="3">
+        <div class="grid-stack-item-content">{"type":"pie","data":{"labels":["Green","Amber","Red"],"datasets":[{"label":"Chart","data":"data","backgroundColor":["green","orange","red"],"borderWidth":1}]},"options":{"scales":{"yAxes":[{"ticks":{"beginAtZero":true}}]}}}</div>
+    </div>
+</div> -->
+
 <script type="text/javascript">
-      GridStack.init();
+    GridStack.init();
 </script>
