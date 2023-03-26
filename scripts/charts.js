d1829297a4b88e4c75b4db715344d87a3079128b
tomljo2000 <66169128+tomljo2000@users.noreply.github.com>
2023-03-26 15:55:56 +0100

    Revert "got graph rendering"
    This reverts commit 191288eb96788f4e9785cca3f5358789ef66f7f9.

--- scripts/charts.js
+++ scripts/charts.js
@@ -74,13 +74,6 @@ window.onload = () => {
       let canvas = document.createElement('canvas')
       canvas.setAttribute('id',item.graph_id)
       gridItem.appendChild(canvas)
-
-      if(item.graph_id === "gauge"){
-        item.graph_data.data.datasets[0].data=data
-        item.graph_data.data.datasets[0].value = value
-        item.graph_data.options.valueLabel.formatter = Math.round 
-      }
-
       
       let ctx = document.getElementById(item.graph_id).getContext('2d');
       window.lineChart = new Chart(ctx, item.graph_data);
@@ -90,3 +83,6 @@ window.onload = () => {
 
   })
 }
+
+
+
