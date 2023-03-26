d60e2613222a2c0cd2cd19224fca4fff8fe3dc11
mRichardio <c2019629@hallam.shu.ac.uk>
2023-03-26 18:06:30 +0100

    Changed history graph

--- scripts/historyChartFusion.js
+++ scripts/historyChartFusion.js
@@ -1,151 +1,110 @@
-FusionCharts.ready(function(){
-    var chartObj = new FusionCharts({
-type: 'multiaxisline',
-renderAt: 'chart-container',
-width: '600',
-height: '350',
-dataFormat: 'json',
-dataSource: {
-"chart": {
-    "caption": "Revenue Analysis",
-    "subcaption": "Last 12 weeks",
-    "xAxisName": "Week of Year",
-    "showValues": "0",
-    "theme": "fusion"
-},
-"categories": [{
-    "category": [{
-        "label": "1"
-    }, {
-        "label": "2"
-    }, {
-        "label": "3"
-    }, {
-        "label": "4"
-    }, {
-        "label": "5"
-    }, {
-        "label": "6"
-    }, {
-        "label": "7"
-    }, {
-        "label": "8"
-    }, {
-        "label": "9"
-    }, {
-        "label": "10"
-    }, {
-        "label": "11"
-    }, {
-        "label": "12"
-    }]
-}],
-"axis": [{
-    "title": "Revenue",
-    "tickWidth": "10",
-    "numberPrefix": "$",
-    "divlineDashed": "1",
-    "dataset": [{
-        "seriesname": "Revenue",
-        "lineThickness": "3",
-        "data": [{
-            "value": "137500"
-        }, {
-            "value": "124350"
-        }, {
-            "value": "156700"
-        }, {
-            "value": "131450"
-        }, {
-            "value": "208300"
-        }, {
-            "value": "219900"
-        }, {
-            "value": "227500"
-        }, {
-            "value": "254300"
-        }, {
-            "value": "155900"
-        }, {
-            "value": "105650"
-        }, {
-            "value": "120950"
-        }, {
-            "value": "127500"
+FusionCharts.ready(function() {
+    var visitChart = new FusionCharts({
+      type: 'msline',
+      renderAt: 'chart-container',
+      width: '700',
+      height: '400',
+      dataFormat: 'json',
+      dataSource: {
+        "chart": {
+          "theme": "fusion",
+          "caption": "Waterman Group",
+          "subCaption": "Action Timeline",
+          "xAxisName": "Day"
+        },
+        "categories": [{
+          "category": [{
+              "label": "Mon"
+            },
+            {
+              "label": "Tue"
+            },
+            {
+              "label": "Wed"
+            },
+            {
+              "vline": "true",
+              "lineposition": "0",
+              "color": "#62B58F",
+              "labelHAlign": "center",
+              "labelPosition": "0",
+              "label": "National holiday",
+              "dashed": "1"
+            },
+            {
+              "label": "Thu"
+            },
+            {
+              "label": "Fri"
+            },
+            {
+              "label": "Sat"
+            },
+            {
+              "label": "Sun"
+            }
+          ]
+        }],
+        "dataset": [{
+            "seriesname": "Bakersfield Central",
+            "data": [{
+                "value": "15123"
+              },
+              {
+                "value": "14233"
+              },
+              {
+                "value": "25507"
+              },
+              {
+                "value": "9110"
+              },
+              {
+                "value": "15529"
+              },
+              {
+                "value": "20803"
+              },
+              {
+                "value": "19202"
+              }
+            ]
+          },
+          {
+            "seriesname": "Los Angeles Topanga",
+            "data": [{
+                "value": "13400"
+              },
+              {
+                "value": "12800"
+              },
+              {
+                "value": "22800"
+              },
+              {
+                "value": "12400"
+              },
+              {
+                "value": "15800"
+              },
+              {
+                "value": "19800"
+              },
+              {
+                "value": "21800"
+              }
+            ]
+          }
+        ],
+        "trendlines": [{
+          "line": [{
+            "startvalue": "17022",
+            "color": "#62B58F",
+            "valueOnRight": "1",
+            "displayvalue": "Average"
+          }]
         }]
-    }]
-}, {
-    "title": "Orders",
-    "axisOnLeft": "0",
-    "numDivlines": "8",
-    "tickWidth": "10",
-    "divlineDashed": "1",
-    "dataset": [{
-        "seriesname": "Orders",
-        "data": [{
-            "value": "22567"
-        }, {
-            "value": "21348"
-        }, {
-            "value": "24846"
-        }, {
-            "value": "19237"
-        }, {
-            "value": "20672"
-        }, {
-            "value": "23403"
-        }, {
-            "value": "30278"
-        }, {
-            "value": "26719"
-        }, {
-            "value": "21940"
-        }, {
-            "value": "24396"
-        }, {
-            "value": "22340"
-        }, {
-            "value": "25439"
-        }]
-    }]
-}, {
-    "title": "Footfalls",
-    "axisOnLeft": "0",
-    "numDivlines": "5",
-    "tickWidth": "10",
-    "numberSuffix": "",
-    "divlineDashed": "1",
-    "dataset": [{
-        "seriesname": "Footfalls",
-        "data": [{
-            "value": "68473"
-        }, {
-            "value": "57934"
-        }, {
-            "value": "78925"
-        }, {
-            "value": "69213"
-        }, {
-            "value": "74892"
-        }, {
-            "value": "81123"
-        }, {
-            "value": "90086"
-        }, {
-            "value": "91174"
-        }, {
-            "value": "68934"
-        }, {
-            "value": "80934"
-        }, {
-            "value": "73634"
-        }, {
-            "value": "84453"
-        }]
-    }]
-}]
-}
-}
-);
-    chartObj.render();
-});
+      }
+    }).render();
+  });
+  
