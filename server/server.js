d5a32f1e1222134f9c39d39a8e65602c3471bdb2
James Westhead <james_westhead@outlook.com>
2023-04-02 19:07:07 +0100

    Revert "NODE.JS VERSION See desc"
    This reverts commit cce84d6f02e6a2fe6958448b5f3872a5be71ee5c.

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 const port = 4001
-const fs = require('fs/promises')
+const fs = require('fs')
 const cors = require('cors')
 
 app.use(cors())
@@ -11,23 +11,11 @@ app.use(express.json())
 
 app.post('/save-graph', (req, res) => {
     console.log(req.body)
-    const path = `../configs/graphs/${req.body.graph_id}.json`
-    fs.readFile(path)
-    .then((data) => {
-      data = JSON.parse(data)
-      console.log(data.data.datasets[0].label)
-
-      data.data.datasets[0].label = req.body.graph_title
-
-      fs.writeFile(path,JSON.stringify(data),(err)=>console.log(err)) 
-    })
-    .catch((error) => {
-      console.log(error)
-    });
+    fetch(`../configs/graph/${}`)
     // console.log(req)
     res.send('Hello World! ')
 })
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
