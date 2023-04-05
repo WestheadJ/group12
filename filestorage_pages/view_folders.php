bdd102e2e26797837a348521f884ad8427f08add
mrahman13 <116874917+mrahman13@users.noreply.github.com>
2023-04-05 15:10:56 +0100

    adding delete button to folders

--- filestorage_pages/view_folders.php
+++ filestorage_pages/view_folders.php
@@ -1,6 +1,16 @@
 <?php
 include("../connections/config.php");
 
+if(isset($_POST['delete_folder'])) {
+  $folder_id = $_POST['folder_id'];
+
+  $delete_folder_sql = "DELETE FROM folders WHERE id='$folder_id'";
+  mysqli_query($conn, $delete_folder_sql);
+
+  $delete_files_sql = "DELETE FROM uploads WHERE folder_id='$folder_id'";
+  mysqli_query($conn, $delete_files_sql);
+}
+
 $folder_sql = "SELECT * FROM folders";
 $folder_result = mysqli_query($conn, $folder_sql);
 ?>
@@ -13,14 +23,16 @@ $folder_result = mysqli_query($conn, $folder_sql);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<h1 class="folders-heading">Folders</h1>
+  <h1 class="folders-heading">Folders</h1>
 
   <?php while($folder_row = mysqli_fetch_assoc($folder_result)) { ?>
-    <p>
     <div class="folder-container">
-    <a href="view_files.php?folder_id=<?php echo $folder_row['id']; ?>" class="folder-link"><?php echo $folder_row['name']; ?></a>
-      </div>
-    </p>
+      <a href="view_files.php?folder_id=<?php echo $folder_row['id']; ?>" class="folder-link"><?php echo $folder_row['name']; ?></a>
+      <form action="" method="POST">
+        <input type="hidden" name="folder_id" value="<?php echo $folder_row['id']; ?>">
+        <input type="submit" name="delete_folder" value="Delete" class="delete-button" onclick="return confirm('Are you sure you want to delete this folder, and the files contained within it?')">
+      </form>
+    </div>
   <?php } ?>
 </body>
 </html>
