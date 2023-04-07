0a968d97e2576b8b9281e7c1210ae011f87514f0
James Westhead <james_westhead@outlook.com>
2023-04-07 12:37:07 +0100

    Changed my con to conn

--- scripts/getGraph.php
+++ scripts/getGraph.php
@@ -2,7 +2,7 @@
 require("../configs/db/dbConnection.php");
 
 $graph_id = $_GET['graph_id'];
-$result = mysqli_query($con, "SELECT * FROM graph WHERE graph_id ='" . $graph_id . "';");
+$result = mysqli_query($conn, "SELECT * FROM graph WHERE graph_id ='" . $graph_id . "';");
 $to_encode = array();
 
 while ($row = mysqli_fetch_assoc($result)) {
