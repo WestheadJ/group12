c5fd2a99fe8beea23398a7a0963cd6d7c0e4b1df
James Westhead <james_westhead@outlook.com>
2023-04-10 12:47:12 +0100

    Add JSON to db

---
+++ scripts/addToDB.php
@@ -0,0 +1,13 @@
+<?php
+// TO RUN: Go to base URL e.g mine is localhost/group12 then add /scripts/addGraphs.php
+
+require("../configs/dbConnection.php");
+
+// UNCOMMENT AND CHANGE THESE VALUES 
+// $json = file_get_contents("../configs/graphs/example.json");
+// $graph_id = example graph id;
+
+// $query = $con->prepare("INSERT INTO graph(graph_id,graph_data) VALUES (?,?)");
+// $query->bind_param("is", $graph_id, $json);
+// $query->execute();
+?>
