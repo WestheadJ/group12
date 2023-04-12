250c9071cda07cfd01f4d66863e426957248b64e
mrahman13 <116874917+mrahman13@users.noreply.github.com>
2023-04-12 13:25:56 +0100

    report button to dashboard/fixing css file storage

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -3,11 +3,16 @@ require 'watermanTop.php';
 
 ?>
 
+
 <script src="../scripts/charts.js"></script>
 <script src="../scripts/editDashboard.js"></script>
+<script src="../scripts/print.js"></script>
+
+<?php include 'report.php'; ?>
 
 <div id="mainpage">
   <div class="toolbar">
+   <!-- <button id="print" class="configure-btn print-button" onclick="printReport()">Print Report</button> -->
     <button id="edit" class="configure-btn" onclick="edit()">Edit Dashbaord</button>
     <button id="editing" class="configure-btn" onclick="save()">Save</button>
     <button id="editing" class="configure-btn" onclick="addChart()">Add chart</button>
---
+++ pages/file.php
@@ -0,0 +1,4 @@
+
+<?php require '../pages/watermanTop.php';?>
+<?php require '../filestorage_pages/filestorage.php';?>
+<?php require '../pages/watermanBottom.php';?>
