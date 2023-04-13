531e19f1760b2f8906804ff969b84a8b20d1142e
tomljo2000 <66169128+tomljo2000@users.noreply.github.com>
2023-04-13 17:50:14 +0100

    added x-axis and y-axis to edit graph

--- pages/editGraph.php
+++ pages/editGraph.php
@@ -9,75 +9,40 @@ require 'watermanTop.php';
 <script src="../scripts/editChart.js"></script>
 <link rel="stylesheet" href="../styles/editGraph.css" />
 
-<h1 id="title">Edit Graph OR Chart</h1>
+<h1 id="title">Edit Chart</h1>
 <div id="main-content">
   <div id="graph-container">
-    <h3>Preview:</h3>
     <canvas id="edit-bar-chart"></canvas>
   </div>
   <div id="form-container">
-    <h3>Options:</h3>
     <form onsubmit="return save(event)" method="post" id="form">
-      <input type="text" id="graph-title" name="graph-title" value="Chart" oninput="updatePreview()">
-      <a href="dashboard.php">Cancel</a>
-      <input id="submit-button" type="submit" value="Save">
+      <div class="formItem">
+        <div class="inputItem">
+          <form action="gridStack.php" method="post">
+            <label>Chart title</label>
+            <input type="text" id="graph-title" name="graph-title" value="Chart" oninput="updatePreview('graph-title')">
+            <label>X Axis</label>
+            <input type="text" id="x-axis" name="x-axis" oninput="updatePreview('x-axis')">
+            <label>Y Axis</label>
+            <input type="text" id="y-axis" name="y-axis" oninput="updatePreview('y-axis')">
+            <!--<label>Graph Type</label>
+            <select id="Graph Type" name="Graph Type">
+              <option value="Bar Chart">Bar Chart</option>
+              <option value="Pie Chart">Pie Chart</option>
+              <option value="Gauge Chart">Gauge Chart</option>
+            </select> -->
+        </div>
+        <a href="dashboard.php">Cancel</a>
+        <input id="submit-button" type="submit" value="Save">
     </form>
   </div>
 </div>
 
-<link rel="stylesheet" href="../styles/editGraph.css">
 
-<!-- JAMES: Need to discuss this friday 07.04 -->
 
-<!-- <div class="main">
-  <div class="container">
-    <div class="item1">
-      <h1>Edit Graph OR Chart</h1>
-
-      <script src="../scripts/editChart.js"></scrip>
-      <div class="graphItem">
-        <canvas id="editBarChart"></canvas>
-      </div>
-
-    </div>
-    <div class="formItem">
-      <div class="inputItem">
-        <form action="gridStack.php" method="post">
-
-          <label>Chart title</label>
-          <input cla type="text" id="gTitle" name="graphTitle" placeholder="Your Graph Title..">
-          <label>X Axis</label>
-          <input type="text" id="xAxis" name="XAxis" placeholder="Your Graph X Axis Label..">
-          <label>Y Axis</label>
-          <input type="text" id="yAxis" name="YAxis" placeholder="Your Graph Y Axis Label..">
-          <label>Graph Type</label> -->
-<!-- JAMES: Will need to be the list of all the graphs we have ( we need more with fake data already in them) -->
-<!-- <select id="Graph Type" name="Graph Type">
-  <option value="Bar Chart">Bar Chart</option>
-  <option value="Pie Chart">Pie Chart</option>
-  <option value="Gauge Chart">Gauge Chart</option>
-</select> -->
-<!-- JAMES: Why is there a graph Description needed? If anything that should be stored on the db and then used as a tooltip when adding graphs...  -->
-<!-- <label>Graph Description</label>
-<textarea id="Graph Description" name="Graph Description" placeholder="Enter any details.."
-  style="height:200px"></textarea>
-</div>
-<div class="buttonItem">
-  <div>
-    <input type="submit" value="Update">
-  </div>
-  <div>
-    <button class="cancel" href="gridStack.php">Cancel</button>
-  </div>
-  <div>
-    <button class="cancel" href="#">Update</button>
-  </div>
-</div>
-</form>
-</div>
 
 </div>
-</div> -->
+</div>
 
 <?php
 require 'watermanBottom.php';
--- scripts/editChart.js
+++ scripts/editChart.js
@@ -54,9 +54,10 @@ function getGraphData() {
       else {
         responseData.data.datasets[0].data = randomDataVar
       }
-
       document.getElementById("graph-title").value = responseData.options.title.text
-     
+      document.getElementById("x-axis").value = responseData.options.scales.xAxes[0].scaleLabel.labelString
+      document.getElementById("y-axis").value = responseData.options.scales.yAxes[0].scaleLabel.labelString
+
       let canvas = document.getElementById("edit-bar-chart")
       var ctx = canvas.getContext('2d')
       graph = new Chart(ctx, responseData)
@@ -65,9 +66,20 @@ function getGraphData() {
 }
 
 // https://stackoverflow.com/questions/7056669/how-to-prevent-default-event-handling-in-an-onclick-method
-function updatePreview() {
-  let currentTitle = document.getElementById('graph-title').value
-  graph.options.title.text = currentTitle
+function updatePreview(id) {
+  let input = document.getElementById(id).value
+
+  switch (id) {
+    case "graph-title":
+      graph.options.title.text = input
+      break;
+    case "x-axis":
+      graph.options.scales.xAxes[0].scaleLabel.labelString = input
+      break;
+    case "y-axis":
+      graph.options.scales.yAxes[0].scaleLabel.labelString = input
+      break;
+  }
   graph.update()
 }
 
@@ -78,7 +90,11 @@ function save(e) {
     .then((responseData) => {
       let json = JSON.parse(responseData[0].graph_data)
       let title = document.getElementById('graph-title').value
+      let xAxis = document.getElementById('x-axis').value
+      let yAxis = document.getElementById('y-axis').value
       json.options.title.text = title
+      json.options.scales.xAxes[0].scaleLabel.labelString = xAxis
+      json.options.scales.yAxes[0].scaleLabel.labelString = yAxis
       fetch('../scripts/saveGraph.php?graph_id=' + graph_id, {
         method: "POST",
         body: JSON.stringify(json)
