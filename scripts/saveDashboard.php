d546278b58cbec0fd0b4a40575beb20272666131
James Westhead <james_westhead@outlook.com>
2023-04-13 03:25:24 +0100

    prepped save dashboard script

---
+++ scripts/saveDashboard.php
@@ -0,0 +1,12 @@
+<?php
+require('../configs/db/dbConnection.php');
+$graph_id = $_GET['dashboard_id'];
+$body = file_get_contents('php://input');
+$sql = "UPDATE dashboard SET dashboard_data='" . $body . "' WHERE dashboard_id=" . $graph_id;
+
+if (mysqli_query($conn, $sql)) {
+    echo json_encode('{"status":200,"message":"updated"}');
+} else {
+    echo json_encode('{"status":500,"message":"Error updating record: ' . (string) mysqli_error($conn) . '"}');
+}
+?>
