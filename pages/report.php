fc78e299d8e105f29c25a81ae8fe5761f6d48888
mrahman13 <116874917+mrahman13@users.noreply.github.com>
2023-04-14 12:22:38 +0100

    fixing printing issue

--- pages/report.php
+++ pages/report.php
@@ -2,8 +2,6 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Report</title>
   <link rel="stylesheet" href="../styles/report.css">
 </head>
@@ -11,22 +9,6 @@
   <div class="container">
     <button id="linkModal">Reports</button>
   </div>
-
-  <div id="modal" class="offmodal">
-    <div class="modal-container">
-      <header>
-        <h2>Reports</h2>
-        <button id="printReport">Print Report</button>
-        <button id="closeModal">Close</button>
-      </header>
-      <div class="modal-body">
-        <table id="report-table"></table>
-      </div>
-    </div>
-  </div>
-
   <script src="../scripts/report.js"></script>
 </body>
 </html>
-
-
--- scripts/report.js
+++ scripts/report.js
@@ -1,49 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-  var modal = document.getElementById('modal');
-  var reportTable = document.getElementById('report-table');
-  var printButton = document.getElementById('printReport');
-  var closeButton = document.getElementById('closeModal');
-
-  document.getElementById('linkModal').addEventListener('click', function () {
-    modal.classList.remove('offmodal');
-    modal.classList.add('onmodal');
-
-    fetch('../configs//report/report.json')
-      .then(response => response.json())
-      .then(data => {
-        var html = '';
-        data.charts.forEach(chart => {
-//https://stackoverflow.com/questions/17120633/loop-through-each-html-table-column-and-get-the-data-using-jquery
-//The source I used to help me implement the forEach() method to loop through each chart in the report.json file
-
-          html += '<tr><th colspan="2">' + chart.chartTitle + '</th></tr>';
-          chart.tableData.forEach(table => {
-            html += '<tr>';
-            html += '<td>' + table.category + '</td>';
-            html += '<td><span class="statusColor" style="background-color:' + table.statusColor + '"></td>';
-            html += '</tr>';
- //https://www.youtube.com/watch?v=eS-FVnhjvEQ
- // The source I used to help me display the graph data in a table. 
-          });
+document.getElementById('linkModal').addEventListener('click', function () {
+  var newWindow = window.open('', '_blank');
+  newWindow.document.write('<html><head><title>Report</title><link rel="stylesheet" href="../styles/report.css"></head><body><button id="printReport">Print Report</button><table id="report-table"></table><div id="modal" class="offmodal"><div class="modal-container"><header><h2>Reports</h2><button id="printReport">Print Report</button></header><div class="modal-body"><table id="report-table"></table></div></div></div></body></html>');
+  var reportTable = newWindow.document.getElementById('report-table');
+  fetch('../configs/report/report.json')
+    .then(response => response.json())
+    .then(data => {
+      var html = '';
+      data.charts.forEach(chart => {
+        html += '<tr><th colspan="2">' + chart.chartTitle + '</th></tr>';
+        chart.tableData.forEach(table => {
+          html += '<tr>';
+          html += '<td>' + table.category + '</td>';
+          html += '<td><span class="statusColor" style="background-color:' + table.statusColor + '"></td>';
+          html += '</tr>';
         });
-        reportTable.innerHTML = html;
       });
-
-  }, false);
-
-  printButton.addEventListener('click', function () {
-    window.print();
-  //https://www.w3schools.com/jsref/met_win_print.asp
-  //The source I used to help me implement a print option for the report. 
+      reportTable.innerHTML = html;
+    });
+  newWindow.document.getElementById('printReport').addEventListener('click', function() {
+    newWindow.print();
   });
-
-  closeButton.addEventListener('click', function () {
-    modal.classList.remove('onmodal');
-    modal.classList.add('offmodal');
-//https://github.com/ArslanAmeer/Ui-Component-Basic-JS-PopUp-Modal/blob/master/source/js/script.js
-//The source I used to help me close and open the modal box. 
-  });
-
-});
+}, false);
 
