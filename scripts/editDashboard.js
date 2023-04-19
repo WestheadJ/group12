01b566b423c64bef9d704f218c499f465310a426
James Westhead <james_westhead@outlook.com>
2023-04-19 02:34:45 +0100

    Saving dashboard works

--- scripts/editDashboard.js
+++ scripts/editDashboard.js
@@ -8,18 +8,17 @@ function edit() {
   for (var i = 0; i < widgets.length; i++) {
     // Remove the delete button from the widget
     removeDeleteButton(widgets[i]);
-  
+
     // Add the delete button to the widget
     addDeleteButton(widgets[i], grid);
-  
+
     // Check if an edit button already exists on this widget
     if (widgets[i].querySelector('.edit-button')) {
       continue;
     }
-  
+
     if (widgets[i].getAttribute('id') && widgets[i].getAttribute('id').split('-')[0] !== "title" && widgets[i].getAttribute('id').split('-')[0] !== "comment") {
       let id = widgets[i].getAttribute('id')
-      console.log(id)
       let editButton = document.createElement("a")
       if (id.split('-')[0] === "comment" || id.split('-')[0] === "title") {
         editButton.setAttribute("href", `editWidget.php?widget_id=${id.split('-')[1]}&content=${id.split('-')[0]}`)
@@ -33,8 +32,8 @@ function edit() {
       widgets[i].appendChild(editButton)
     }
   }
-  
-  }
+
+}
 
 function addDeleteButton(widget, grid) {
   // Check if the widget already has a delete button
@@ -76,19 +75,19 @@ function addChart() {
     x: 0, y: 5, w: 6, h: 5, content: '<canvas id="dummy"></canvas>' // here
     // gs-h="3" gs-w="1" gs-x="0" gs-y="5"
   });
-  
-  
+
+
   var randomData = function () {
-    return [      
+    return [
       randomScalingFactor(),
       randomScalingFactor(),
       randomScalingFactor()
     ]
   };
-  
+
   // Get the new widget
   var newWidget = document.querySelector('.grid-stack-item:last-child');
-  
+
   // Assings id to the new widget
   newWidget.setAttribute("id", `${widgetCount}`)
 
@@ -125,7 +124,7 @@ function addTitle() {
   addDeleteButton(newWidget, grid);
 }
 // When the DOM is ready...
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // Add delete buttons to existing title widgets
   var existingTitles = document.querySelectorAll('.title-editing');
   for (var i = 0; i < existingTitles.length; i++) {
@@ -134,7 +133,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Add click event listener to the add title button
   var addTitleButton = document.getElementById('add-title-button');
-  addTitleButton.onclick = function() {
+  addTitleButton.onclick = function () {
     // Add the new title widget
     addTitle();
 
@@ -165,39 +164,103 @@ function addComment() {
   if (!existingDeleteButton) {
     addDeleteButton(newWidget, grid);
   }
-
-  
 }
 
-
-
 function save() {
-  menueToggle()
-  //NEED TO BE ABLE TO SAVE DASHBOARD TO DB HERE
-  var grid = GridStack.init();
-  grid.setStatic(true);
-  var deleteButtons = document.getElementsByClassName('delete-widget');
-  while (deleteButtons[0]) {
-    deleteButtons[0].parentNode.removeChild(deleteButtons[0]);
+
+  var gridstackItems = document.querySelectorAll('.grid-stack-item')
+
+  let dashboardJSON = {
+    "userID": "watermangrp",
+    configs: []
   }
+  gridstackItems.forEach((item, i) => {
+    console.log(item)
+    var gs_h = item.getAttribute("gs-h")
+    var gs_w = item.getAttribute("gs-w")
+    var gs_x = item.getAttribute("gs-x")
+    var gs_y = item.getAttribute("gs-y")
+    var id = item.getAttribute("id")
+    var contentType;
+    var content;
+    if (id.split('-')[0] === "comment") {
+      contentType = "comment"
+      content = {
+        comment: item.firstChild.innerHTML
+      }
 
-  var editButtons = document.getElementsByClassName('edit-button');
-  while (editButtons[0]) {
-    editButtons[0].parentNode.removeChild(editButtons[0])
-  }
-  alert('dashboard saved')
+    }
+    else if (id.split('-')[0] === "title") {
+      contentType = "title"
+      content =
+      {
+        title: item.firstChild.innerHTML
+      }
 
-  // Toggles between CSS classes for static and live state
-  var comment = document.querySelectorAll('[class=comment-editing]');
-  comment.forEach(comment => {
-    comment.setAttribute("class", `comment-saved`);
-  });
+    }
+    else {
+      contentType = "graph"
+      content =
+      {
+        graph_id: id.split('-')[1]
+      }
 
-  // Toggles between CSS classes for static and live state
-  var title = document.querySelectorAll('[class=title-editing]');
-  title.forEach(title => {
-    title.setAttribute("class", `title-saved`);
-  });
+
+    }
+    console.log(content)
+    dashboardJSON.configs.push({
+      widget_id: i + 1,
+      gs_h: gs_h,
+      gs_w: gs_w,
+      gs_x: gs_x,
+      gs_y: gs_y,
+      content: content
+    })
+
+
+  })
+
+  fetch("../scripts/saveDashboard.php?dashboard_id=1", { method: "post", body: JSON.stringify(dashboardJSON) })
+    .then(res => res.json())
+    .then(json => {
+      json = JSON.parse(json)
+      if (json.status === 200) {
+
+        //NEED TO BE ABLE TO SAVE DASHBOARD TO DB HERE
+        var grid = GridStack.init();
+        grid.setStatic(true);
+        var deleteButtons = document.getElementsByClassName('delete-widget');
+        while (deleteButtons[0]) {
+          deleteButtons[0].parentNode.removeChild(deleteButtons[0]);
+        }
+
+        var editButtons = document.getElementsByClassName('edit-button');
+        while (editButtons[0]) {
+          editButtons[0].parentNode.removeChild(editButtons[0])
+        }
+
+        // Toggles between CSS classes for static and live state
+        var comment = document.querySelectorAll('[class=comment-editing]');
+        comment.forEach(comment => {
+          comment.setAttribute("class", `comment-saved`);
+        });
+
+        // Toggles between CSS classes for static and live state
+        var title = document.querySelectorAll('[class=title-editing]');
+        title.forEach(title => {
+          title.setAttribute("class", `title-saved`);
+        });
+        alert("Dashboard saved")
+        menueToggle()
+      }
+      else {
+        alert("There was an error saving the dashboard")
+      }
+    })
+
+  console.log(dashboardJSON)
+
+
 
 }
 
--- scripts/saveDashboard.php
+++ scripts/saveDashboard.php
@@ -1,8 +1,8 @@
 <?php
 require('../configs/db/dbConnection.php');
-$graph_id = $_GET['dashboard_id'];
+$dashboardId = $_GET['dashboard_id'];
 $body = file_get_contents('php://input');
-$sql = "UPDATE dashboard SET dashboard_data='" . $body . "' WHERE dashboard_id=" . $graph_id;
+$sql = "UPDATE dashboard SET dashboard_data='" . $body . "' WHERE dashboard_id=" . $dashboardId;
 
 if (mysqli_query($conn, $sql)) {
     echo json_encode('{"status":200,"message":"updated"}');
