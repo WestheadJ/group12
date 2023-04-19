04a849f428aaa0d0469a50a22a72ec7843031c53
tomljo2000 <66169128+tomljo2000@users.noreply.github.com>
2023-04-20 00:42:43 +0100

    a

--- scripts/getGraphCount.php
+++ scripts/getGraphCount.php
@@ -1,20 +1,13 @@
 <?php
 require("../configs/db/dbConnection.php");
 
-$graph_id = $_GET['graph_id'];
-$result = mysqli_query($conn, "SELECT COUNT(graph_id) as f from graph");
+$result = mysqli_query($conn, "SELECT COUNT(graph_id) as x from graph");
 $to_encode = array();
 
-// while ($row = mysqli_fetch_assoc($result)) {
-//     $to_encode[] = $row;
-// }
+while ($row = mysqli_fetch_assoc($result)) {
+    $to_encode[] = $row;
+}
 
-$stmt = $conn->prepare("SELECT COUNT(graph_id) as x from graph");
-$stmt->execute();
-$result = $stmt->get_result();
-$obj = $result->fetch_object();
-$graph_count = $obj->f;
-
-echo json_encode($graph_count);
+echo json_encode($to_encode);
 
 ?>
