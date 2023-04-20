92a091f3a2a3908ae8285cd1ab3eba0c5fb0b5ea
mRichardio <c2019629@hallam.shu.ac.uk>
2023-04-20 02:01:50 +0100

    k

--- scripts/report.js
+++ scripts/report.js
@@ -1,28 +1,29 @@
 document.getElementById('linkModal').addEventListener('click', function () {
   var newWindow = window.open('', '_blank');
-  var htmlCode = `
-  <html>
-    <head>
-      <title>Report</title>
-      <link rel="stylesheet" href="../styles/report.css">
-    </head>
-    <body>
-      <button id="printReport">Print Report</button>
-      <table id="report-table"></table>
-      <div id="modal" class="offmodal">
-        <div class="modal-container">
-          <header>
-            <h2>Reports</h2>
-            <button id="printReport">Print Report</button>
-          </header>
-          <div class="modal-body">
-            <table id="report-table"></table>
-          </div>
-        </div>
-      </div>
-    </body>
-  </html>
-`;
+  newWindow.document.write('<html><head><title>Report</title><link rel="stylesheet" href="../styles/report.css"></head><body><button id="printReport">Print Report</button><table id="report-table"></table><div id="modal" class="offmodal"><div class="modal-container"><header><h2>Reports</h2><button id="printReport">Print Report</button></header><div class="modal-body"><table id="report-table"></table></div></div></div></body></html>');
+//   var htmlCode = `
+//   <html>
+//     <head>
+//       <title>Report</title>
+//       <link rel="stylesheet" href="../styles/report.css">
+//     </head>
+//     <body>
+//       <button id="printReport">Print Report</button>
+//       <table id="report-table"></table>
+//       <div id="modal" class="offmodal">
+//         <div class="modal-container">
+//           <header>
+//             <h2>Reports</h2>
+//             <button id="printReport">Print Report</button>
+//           </header>
+//           <div class="modal-body">
+//             <table id="report-table"></table>
+//           </div>
+//         </div>
+//       </div>
+//     </body>
+//   </html>
+// `;
   var reportTable = newWindow.document.getElementById('report-table');
   fetch('../configs/report/report.json')
     .then(response => response.json())
